061df5893abda926d2f23fb836ec60a497844522
Eric Johnson <student@epicodus.com>
2014-08-13 13:41:29 -0700

    Refactor Doctor.all

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -9,5 +9,14 @@ class Doctor
 
   def self.all
     doctors = []
+    results = DB.exec("SELECT * FROM doctors;")
+    results.each do |result|
+      attributes = {
+        :id => result['id'],
+        :name => result['name']
+      }
+      doctors << Doctor.new(attributes)
+    end
+    doctors
   end
 end
