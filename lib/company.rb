01602fe55f60c6c4e1c85915367462bdafe046bf
Eric Johnson <student@epicodus.com>
2014-08-13 15:41:04 -0700

    Refactor show all companies

--- lib/company.rb
+++ lib/company.rb
@@ -9,5 +9,14 @@ class Company
 
   def self.all
     companies = []
+    results = DB.exec("SELECT * FROM companies;")
+    results.each do |result|
+      attributes = {
+        :id => result['id'].to_i,
+        :name => result['name']
+      }
+      companies << Company.new(attributes)
+    end
+    companies
   end
 end
